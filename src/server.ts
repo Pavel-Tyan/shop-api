8d22e533173cde0603631b59a900a990dcd0e0f5
Tyan_Pavel <ptyan_1@edu.hse.ru>
2025-02-12 20:43:00 +0300

    feat: add server.ts for launching app
    server.ts is entry point for app

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { errorHandler } from "./utils/errorHandler";
+import { categoryRouter } from "./routes/category.route";
+import { dataSource } from "./app-data-source";
+import { productRouter } from "./routes/product.route";
+
+dataSource
+  .initialize()
+  .then(() => {
+    console.log("Инициализация для БД прошла успешно");
+  })
+  .catch((err) => {
+    console.error("Ошибка во время инициализации для БД", err);
+  });
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/products", productRouter);
+app.use("/api/categories", categoryRouter);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
