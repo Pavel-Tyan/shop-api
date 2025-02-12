7d0112c163250f9fa36679173029714c8b6fcfd5
Tyan_Pavel <ptyan_1@edu.hse.ru>
2025-02-12 20:42:51 +0300

    feat: add product route

---
+++ src/routes/product.route.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/product.controller";
+
+export const productRouter = express.Router();
+
+productRouter.post("/", createProduct);
+productRouter.get("/", getProducts);
+productRouter.get("/:id", getProductById);
+productRouter.put("/:id", updateProduct);
+productRouter.delete("/:id", deleteProduct);
