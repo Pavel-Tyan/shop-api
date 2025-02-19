51826770e849d3628910edd809dde842bd54e69f
Tyan_Pavel <paveltyan032@gmail.com>
2025-02-19 22:11:39 +0300

    fix: change errorHandler signature

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,20 +1,31 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { RequestError } from "./RequestError";
 
-export const errorHandler = (err: Error, _: Request, res: Response) => {
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
   console.error(err);
 
   if (err instanceof RequestError) {
-    res.status(err.statusCode).json({
-      status: "error",
-      message: err.message,
-    });
+    res
+      .status(err.statusCode)
+      .json({
+        status: "error",
+        message: err.message,
+      })
+      .end();
 
     return;
   }
 
-  res.status(500).json({
-    status: "error",
-    message: "Internal Server Error",
-  });
+  res
+    .status(500)
+    .json({
+      status: "error",
+      message: "Internal Server Error",
+    })
+    .end();
 };
